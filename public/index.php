74a46529fbfcf80cb83e7d5a42279f49a4dfc591
3a732037 <3a732037@gm.student.ncut.edu.tw>
2020-10-14 02:13:46 +0800

    測試monolog的使用

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+//第三周
+
 require '../vendor/autoload.php';
 use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
@@ -9,3 +11,16 @@ $lara= new Lara();
 $vincent= new Hello\Someone('Vincent');
 $mary= new \Demo\Hello\Someone('Mary');
 $john= new Demo\Hello\Someone('John');
+
+//第四周 monolog練習
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
